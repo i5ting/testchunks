1c7214cc1cefc5479dee5e970f166ba78da9bfe8
i5ting <i5ting@126.com>
2016-12-15 16:53:02 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const http = require('http')
 const request = require('request')
+const debug = require('debug')('testchunks')
+
 const defaultConfig = { 
   method: 'GET'
   , gzip: true
@@ -18,16 +20,20 @@ module.exports = function(app, path, cb, opts){
     
     opts = Object.assign(defaultConfig, opts)
     
-    console.log(opts.url)
+    debug(opts)
     
     // 访问路径
     request(opts
     , function (error, response, body) {
+        debug('error: ' + error)
+        debug('response: ' + response)
+        debug('body: ' + body)
+        debug('chunks array: ' + chunks)
         cb(chunks, error, response, body)
       }
     ).on('data', function(data) {
       // decompressed data as it is received
-      // console.log('decoded chunk: ' + data)
+      debug('decoded chunk: ' + data)
       chunks.push(data.toString())
     })
   })
